d17fc411774b1d152c8f31f89d5ea8777b679cb0
Alino4ka0599 <115590007+Alino4ka0599@users.noreply.github.com>
2023-02-04 01:43:58 +0300

    Add files via upload

---
+++ Лабораторная работа 3/main.py
@@ -0,0 +1,43 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+
+        if not isinstance(pages, int):
+            raise TypeError("pages needs int type")
+
+        self.pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.pages})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+
+        if not isinstance(duration, float):
+            raise TypeError("duration needs float type")
+
+        self.duration = duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration})"
+
+
+if __name__ == "__main__":
+    p_b = PaperBook("1984", "Oruel", 345)
+    print(p_b)
+    print(repr(p_b))
